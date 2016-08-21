5bd7eaa82adff10511c64bb28f5bd34bb9413063
Austin Schleder <ajsch719@gmail.com>
2016-08-20 20:12:11 -0700

    Roster assignments and weekly ranking calculations

--- example3.py
+++ example3.py
@@ -3,26 +3,43 @@ import models3 as fs
 import pandas as pd
 import numpy as np
 
-LEAGUE_SIZE = 10
-ROSTER_SLOTS = ['qb1', 'rb1', 'rb2', 'wr1', 'wr2', 'wr3', 'te1']
+#LEAGUE_SIZE = 10
+#ROSTER_POSITIONS = ['qb', 'rb', 'wr', 'te']
+#ROSTER_SLOTS = ['qb1', 'rb1', 'rb2', 'wr1', 'wr2', 'wr3', 'te1']
+#MINIMUM_GAMES_PLAYED = 0
+#SEASON_LENGTH = 16
+LEAGUE_SIZE = 4
+ROSTER_POSITIONS = ['qb']
+ROSTER_SLOTS = ['qb1', 'qb2']
 SCORING_CATEGORIES = ['pass_yards', 'pass_tds', 'pass_ints', 'rush_yards', 'rush_tds', 'recs', 'rec_yards', 'rec_tds']
 SCORING_VALUES = [0.05, 6.0, -2.0, 0.1, 6.0, 0.0, 0.1, 6.0]
 MINIMUM_GAMES_PLAYED = 0
-SEASON_LENGTH = 16
+SEASON_LENGTH = 5
+NUM_SEASONS = 3
 NICKNAMES = ['Raiders', 'Seahawks', 'Colts', 'Chiefs', 'Chargers', 'Broncos', 'Cardinals', 'Packers', 'Bills', 'Rams', 'Bears', 'Falcons']
 
 gl = pd.read_csv('2015_nfl_weekly_stats.csv')
 gl['position'] = np.where(gl['position'] == 10.0, 'qb', np.where(gl['position'] == 20.0, 'rb', np.where(gl['position'] == 30.0, 'wr', np.where(gl['position'] == 40.0, 'te', 'other'))))
 GAME_LOGS = gl
 
+## Create Player_DB
 player_db = fs.Player_DB(gl, SCORING_CATEGORIES, SCORING_VALUES)
-player_db.set_tiers(['qb', 'rb', 'wr', 'te'], LEAGUE_SIZE, MINIMUM_GAMES_PLAYED)
+player_db.set_tiers(ROSTER_POSITIONS, LEAGUE_SIZE, MINIMUM_GAMES_PLAYED)
 print(player_db)
 
-league1 = fs.League(players=player_db.get_player_pool(ROSTER_SLOTS), teams=fs.create_teams(LEAGUE_SIZE, NICKNAMES), seasons=[])
+## Create a League
+players = player_db.get_player_pool(ROSTER_SLOTS)
+teams = fs.create_teams(LEAGUE_SIZE, NICKNAMES)
+seasons = fs.create_seasons(NUM_SEASONS, SEASON_LENGTH)
+league1 = fs.League(players, teams, seasons, ROSTER_SLOTS, SEASON_LENGTH)
 print(league1)
+print(league1.get_player_by_id('P090'))
+[t.print_scoring_output() for t in league1.teams]
+print(league1.get_season_by_id('S0002').team_rankings)
 
-[print(p) for p in sorted(league1.get_position_players('wr'), key=lambda x: x.position_rank)]
+print(fs.numpy_rank([50, 30, 20, 40]))
+
+#[print(p) for p in sorted(league1.get_position_players('qb'), key=lambda x: x.position_rank)]
 #[print(t) for t in league1.teams[:5]]
 
 #qb1s = player_db.get_position_tier('qb1', LEAGUE_SIZE, MINIMUM_GAMES_PLAYED)
--- models3.py
+++ models3.py
@@ -18,19 +18,80 @@ class League:
     """
     id = 1
     
-    def __init__(self, players, teams, seasons):
-        self.league_id = League.id
+    def __init__(self, players, teams, seasons, roster_slots, season_length):
+        self.league_id = 'L' + str(League.id).zfill(2)
         League.id += 1
         self.players = players
         self.teams = teams
         self.seasons = seasons
+        self.roster_slots = roster_slots
+        self.season_length = season_length
+
+        self.set_rosters()
+        self.generate_player_scores()
+        self.calculate_team_scores()
+        self.calculate_season_stats()
 
     def __str__(self):
         return '{} {}: {} players, {} teams, {} seasons'.format(self.__class__.__name__, self.league_id, len(self.players), len(self.teams), len(self.seasons))
 
+    def get_player_by_id(self, player_id):
+        return [p for p in self.players if p.player_id == player_id][0]
+
+    def get_team_by_id(self, team_id):
+        return [t for t in self.teams if t.team_id == team_id][0]
+
+    def get_season_by_id(self, season_id):
+        return [s for s in self.seasons if s.season_id == season_id][0]
+
     def get_position_players(self, position):
         position_players = [p for p in self.players if p.position == position]
         return position_players
+
+    def set_rosters(self):
+        seasons = self.seasons
+        teams = self.teams
+        players = self.players
+        roster_slots = self.roster_slots
+        for s in seasons:
+            rosters = {t.team_id: [] for t in teams}
+            num_teams = len(teams)
+            for slot in roster_slots:
+                slot_players = get_slot_players(players, slot)
+                assignments = zip(teams, slot_players)
+                [rosters[t.team_id].append(p.player_id) for t, p in assignments]
+                [p.update_team_assignment(s.season_id, t.team_id) for t, p in assignments]
+            s.rosters = rosters
+            [t.update_player_assignments(s.season_id, rosters[t.team_id]) for t in teams]
+
+    def generate_player_scores(self):
+        seasons = self.seasons
+        players = self.players
+        for s in seasons:
+            season_length = s.season_length
+            for p in players:
+                p.update_scoring_output(s.season_id, [random.choice(p.scoring_array) for i in xrange(season_length)])
+
+    def calculate_team_scores(self):
+        seasons = self.seasons
+        teams = self.teams
+        players = self.players
+        for s in seasons:
+            season_length = s.season_length
+            for t in teams:
+                weekly_output = [sum(p.scoring_output[s.season_id][i] for p in players if p.team_assignments[s.season_id] == t.team_id) for i in xrange(season_length)]
+                t.update_scoring_output(s.season_id, weekly_output)
+
+    def calculate_season_stats(self):
+        seasons = self.seasons
+        teams = self.teams
+        for s in seasons:
+            season_length = s.season_length
+            league_size = len(teams)
+            matchup_rankings_by_week = [numpy_rank([t.scoring_output[s.season_id][i] for t in teams]) for i in xrange(season_length)]
+            matchup_rankings_by_team = map(list, zip(*matchup_rankings_by_week))
+            [s.update_team_rankings_weekly(teams[i].team_id, matchup_rankings_by_team[i]) for i in xrange(league_size)]
+
 
 class Player_DB:
     """
@@ -41,7 +102,7 @@ class Player_DB:
     id = 1
 
     def __init__(self, game_logs, scoring_categories, scoring_values):
-        self.db_id = Player_DB.id
+        self.db_id = 'DB' + str(Player_DB.id)
         Player_DB.id += 1
         pdb = analyze_game_logs(game_logs, scoring_categories, scoring_values)
         self.players = create_players_from_pdb(pdb)
@@ -50,29 +111,13 @@ class Player_DB:
     def __str__(self):
         return '{}: {} player(s)'.format(self.__class__.__name__, len(self.players))
 
-    def get_position_tier(self, slot, league_size, minimum_games_played):
-        pdb = self.players
-        pos, tier = convert_slot_to_position(slot)
-        tier_start = (tier-1)*league_size
-        tier_end = (tier)*league_size
-        position_players = [p for p in pdb if (p.position == pos) & (p.gp >= minimum_games_played)]
-        position_players.sort(key=lambda x: x.points_per_gp, reverse=True)
-        position_tier = position_players[tier_start:tier_end]
-        [p.set_slot(slot) for p in position_tier]
-        return position_tier
-
     def set_tiers(self, positions, league_size, minimum_games_played):
         pdb = self.players
-        tier_size = league_size
-        pdb = [p for p in pdb if p.gp >= minimum_games_played]
+        pdb_qualified = [p for p in pdb if p.gp >= minimum_games_played]
         for pos in positions:
-            position_players = [p for p in pdb if p.position == pos]
-            num_players = len(position_players)
-            position_players.sort(key=lambda x: x.points_per_gp, reverse=True)
-            tier_assignments = [(t+1)/tier_size + 1 for t in range(num_players)]
-            slot_assignments = [convert_position_to_slot(pos, t) for t in tier_assignments]
-            [position_players[i].set_slot(slot_assignments[i]) for i in range(num_players)]
-            [position_players[i].set_position_rank(i+1) for i in range(num_players)]
+            position_players = [p for p in pdb_qualified if p.position == pos]
+            position_players_ranked = sort_array_descending(position_players, 'points_per_gp')
+            set_position_tiers(position_players_ranked, pos, league_size)
         self.has_tiers = True
 
     def get_player_pool(self, roster_slots):
@@ -93,7 +138,7 @@ class Player:
     id = 1
 
     def __init__(self, name, position, scoring_array):
-        self.player_id = Player.id
+        self.player_id = 'P' + str(Player.id).zfill(3)
         Player.id += 1
         self.name = name
         self.position = position
@@ -105,15 +150,23 @@ class Player:
         self.consistency = self.utility/self.points_per_gp
         self.slot = position
         self.position_rank = 0
+        self.scoring_output = {}
+        self.team_assignments = {}
 
     def __str__(self):
-        return '{} {:3d}: {:20s} ({}) -- {:.2f} = {:4.2f} / {:4.2f}'.format(self.__class__.__name__, self.player_id, self.name, self.slot, self.consistency, self.utility, self.points_per_gp)
+        return '{} {}: {:20s} ({}) -- {:.2f} = {:5.2f} / {:5.2f} -- {}'.format(self.__class__.__name__, self.player_id, self.name, self.slot, self.consistency, self.utility, self.points_per_gp, self.team_assignments)
 
     def set_slot(self, slot):
         self.slot = slot
 
     def set_position_rank(self, position_rank):
         self.position_rank = position_rank
+
+    def update_team_assignment(self, season_id, team_id):
+        self.team_assignments.update({season_id: team_id})
+
+    def update_scoring_output(self, season_id, seasonal_scoring):
+        self.scoring_output.update({season_id: seasonal_scoring})
 
 class Team:
     """
@@ -124,12 +177,23 @@ class Team:
     id = 1
 
     def __init__(self, nickname):
-        self.team_id = Team.id
+        self.team_id = 'T' + str(Team.id).zfill(2)
         Team.id += 1
         self.nickname = nickname
+        self.player_assignments = {}
+        self.scoring_output = {}
 
     def __str__(self):
-        return '{} {}: {}'.format(self.__class__.__name__, self.team_id, self.nickname)
+        return '{} {}: {} -- {}'.format(self.__class__.__name__, self.team_id, self.nickname, self.scoring_output)
+
+    def update_player_assignments(self, season_id, roster):
+        self.player_assignments.update({season_id: roster})
+
+    def update_scoring_output(self, season_id, seasonal_scoring):
+        self.scoring_output.update({season_id: seasonal_scoring})
+
+    def print_scoring_output(self):
+        print(self.team_id, ' '.join([str(x) for x in self.scoring_output['S0002']]))
 
 class Season:
     """
@@ -137,9 +201,37 @@ class Season:
     """
     id = 1
 
-    def __init__(self):
-        self.season_id = Season.id
+    def __init__(self, season_length):
+        self.season_id = 'S' + str(Season.id).zfill(4)
         Season.id += 1
+        self.season_length = season_length
+        self.rosters = {}
+        self.team_rankings = {}
+
+    def __str__(self):
+        return '{} {}: {} teams'.format(self.__class__.__name__, self.season_id, len(self.rosters))
+
+    def set_rosters(self, teams, players, roster_slots):
+        rosters = {t.team_id: [] for t in teams}
+        num_teams = len(teams)
+        for slot in roster_slots:
+            slot_players = [p for p in players if p.slot == slot]
+            random.shuffle(slot_players)
+            assignments = zip(teams, slot_players)
+            [rosters[t.team_id].append(p.player_id) for t, p in assignments]
+            [p.update_team_assignment(self.season_id, t.team_id) for t, p in assignments]
+        return rosters
+
+    def generate_player_scores(self, players):
+        season_length = self.season_length
+        season_id = self.season_id
+        [p.update_scoring_output(season_id, [random.choice(p.scoring_array) for i in xrange(season_length)]) for p in players]
+
+    def update_team_rankings_weekly(self, team_id, matchup_rankings_by_team):
+        self.team_rankings.update({team_id: matchup_rankings_by_team})
+
+    def print_team_rankings(self):
+        [print(' '.join([str(x) for x in self.team_rankings[i]])) for i in ['T01', 'T02', 'T03', 'T04']]
 
 ###############
 ## Functions ##
@@ -159,11 +251,44 @@ def create_teams(league_size, nicknames):
     teams = [Team(nicknames[t]) for t in xrange(league_size)]
     return teams
 
+def create_seasons(num_seasons, season_length):
+    seasons = [Season(season_length) for i in xrange(num_seasons)]
+    return seasons
+
+def set_position_tiers(players, position, league_size):
+    num_players = len(players)
+    tier_assignments = [t/league_size + 1 for t in range(num_players)]
+    slot_assignments = [convert_position_to_slot(position, t) for t in tier_assignments]
+    [players[i].set_slot(slot_assignments[i]) for i in range(num_players)]
+    [players[i].set_position_rank(i+1) for i in range(num_players)]
+
 def get_certainty_equivalent(scoring_array):
     'Used in Player class to evaluate consistency'
     gp = len(scoring_array)
     ce = np.expm1(sum(np.log1p(scoring_array))/gp)
     return ce
+
+def sort_array_descending(array, key):
+    if key == 'points_per_gp':
+        array.sort(key=lambda x: x.points_per_gp, reverse=True)
+    elif key == 'utility':
+        array.sort(key=lambda x: x.utility, reverse=True)
+    elif key == 'consistency':
+        array.sort(key=lambda x: x.consistency, reverse=True)
+    elif key == 'points':
+        array.sort(key=lambda x: x.points, reverse=True)
+    elif key == 'gp':
+        array.sort(key=lambda x: x.gp, reverse=True)
+    return array
+
+def numpy_rank(array):
+    return np.array(array).argsort().argsort()
+
+def get_slot_players(players, slot, method='shuffle'):
+    slot_players = [p for p in players if p.slot == slot]
+    if method == 'shuffle':
+        random.shuffle(slot_players)
+    return slot_players
 
 def convert_slot_to_position(slot):
     position = slot[:2]
